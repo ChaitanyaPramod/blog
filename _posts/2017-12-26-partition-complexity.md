e97d280dd9f65a57c8faa6307e9677c45e7cc3e9
Chaitanya Pramod <me@chaitanyapramod.com>
2018-01-01 02:43:44 +0530

    Copy posts from previous blog site

---
+++ _posts/2017-12-26-partition-complexity.md
@@ -0,0 +1,7 @@
+---
+title:  "Partition Complexity"
+date:   2017-12-26 00:30:00 +0530
+external_url: https://www.facebook.com/notes/kent-beck/one-bite-at-a-time-partitioning-complexity/1716882961677894/
+permalink: /partition-complexity
+published: false
+---
---
+++ doc/assets/js/lunr-en.js
@@ -0,0 +1,112 @@
+var idx = lunr(function () {
+  this.field('title')
+  this.field('excerpt')
+  this.field('categories')
+  this.field('tags')
+  this.ref('id')
+
+  
+  
+    
+    
+      this.add({
+          title: "First Post",
+          excerpt: "Phew, just finished the basic setup of my brand-new blog-site. It took almost five hours to get here. Working with...",
+          categories: [],
+          tags: [],
+          id: 0
+      })
+      
+    
+      this.add({
+          title: "Viral Loop & the Lean Startup",
+          excerpt: "Disruption of internet service can make you do things you wouldn’t have imagined doing anytime soon. For me this weekend,...",
+          categories: ["books"],
+          tags: [],
+          id: 1
+      })
+      
+    
+  
+});
+
+console.log( jQuery.type(idx) );
+
+var store = [
+  
+    
+    
+    
+      
+      {
+        "title": "First Post",
+        "url": "https://blog.chaitanyapramod.com/first-post",
+        "excerpt": "Phew, just finished the basic setup of my brand-new blog-site. It took almost five hours to get here. Working with...",
+        "teaser":
+          
+            null
+          
+      },
+    
+      
+      {
+        "title": "Viral Loop & the Lean Startup",
+        "url": "https://blog.chaitanyapramod.com/viral-loop-and-the-lean-startup",
+        "excerpt": "Disruption of internet service can make you do things you wouldn’t have imagined doing anytime soon. For me this weekend,...",
+        "teaser":
+          
+            null
+          
+      }
+    
+  ]
+
+$(document).ready(function() {
+  $('input#search').on('keyup', function () {
+    var resultdiv = $('#results');
+    var query = $(this).val().toLowerCase().replace(":", "");
+    var result =
+      idx.query(function (q) {
+        query.split(lunr.tokenizer.separator).forEach(function (term) {
+          q.term(term, {  boost: 100 })
+          if(query.lastIndexOf(" ") != query.length-1){
+            q.term(term, {  usePipeline: false, wildcard: lunr.Query.wildcard.TRAILING, boost: 10 })
+          }
+          if (term != ""){
+            q.term(term, {  usePipeline: false, editDistance: 1, boost: 1 })
+          }
+        })
+      });
+    resultdiv.empty();
+    resultdiv.prepend('<p class="results__found">'+result.length+' Result(s) found</p>');
+    for (var item in result) {
+      var ref = result[item].ref;
+      if(store[ref].teaser){
+        var searchitem =
+          '<div class="list__item">'+
+            '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
+              '<h2 class="archive__item-title" itemprop="headline">'+
+                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
+              '</h2>'+
+              '<div class="archive__item-teaser">'+
+                '<img src="'+store[ref].teaser+'" alt="">'+
+              '</div>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt+'</p>'+
+            '</article>'+
+          '</div>';
+      }
+      else{
+    	  var searchitem =
+          '<div class="list__item">'+
+            '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
+              '<h2 class="archive__item-title" itemprop="headline">'+
+                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
+              '</h2>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt+'</p>'+
+            '</article>'+
+          '</div>';
+      }
+      resultdiv.append(searchitem);
+    }
+  });
+});
